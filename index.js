b60eddf1542e1201e781c4dce3b5eacd2ee9a35a
hydrogen-studio <hydrogen.studio.llc@gmail.com>
2020-10-15 09:44:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const lite = require("dlite.js");
+let client = new lite.Bot({ fetchAllUsers: true, logging: false })
+ const shard = new lite.shardService()
 
+client.on("messageCreate", (msg) => {
+    if(msg.author.bot)return
+
+    if(msg.content == "dl.test"){
+        let embed = new lite.CreateEmbed()
+            .setTitle("Test!")
+            .setDescription(shard.getShard(0).users.data.length)
+            .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+            .setColor("RANDOM");
+        msg.channel.send(embed)
+    }
+})
+
+client.on("channelUpdate", function(msg){
+    console.log(msg)
+})
+
+client.ready(function(){
+    // console.log(client.shardID)
+    msg.channel.send("Ready!")
+})
+
+client.login("Your bot token!")
